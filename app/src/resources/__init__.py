a330eee77a496b4048588144f47af5c034c91214
Miroslava Škutová <skutova.mir@outlook.com>
2023-11-17 17:41:22 +0100

    Test MongoDB endpoint

--- app/src/resources/__init__.py
+++ app/src/resources/__init__.py
@@ -1,7 +1,10 @@
 from src.resources.internal import *
-
+from src.resources.mongo import *
 
 def register_resources(api):
     # Internal
     api.add_resource(HealthCheckResource, "/healthcheck")
     api.add_resource(HealthCheckDatabaseResource, "/healthcheck_db")
+
+    # MongoDB
+    api.add_resource(MongoTestResource, "/test")
